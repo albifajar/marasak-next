5353150fcab0136dbdb91ac7732f63cfb654fcdc
winatapranatadev <winatapranata.dev@gmail.com>
2022-10-08 14:22:17 +0700

    feat: speech to text feture

--- components/Forms/Search/index.jsx
+++ components/Forms/Search/index.jsx
@@ -1,15 +1,24 @@
 import { MicrophoneIcon, SearchIcon } from '@components/Icons'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 
 export const Search = () => {
+  const { transcript, listening } = useSpeechRecognition()
+
   return (
-    <div className="relative flex items-center text-black">
-      <button className="absolute left-4">
-        <SearchIcon />
-      </button>
-      <input type="text" className="w-full px-12 py-3 border border-gray-400 rounded-md focus:border-blue-200" />
-      <button className="absolute right-2">
-        <MicrophoneIcon />
-      </button>
-    </div>
+    <>
+      <div className="relative flex items-center text-black">
+        <button className="absolute left-4">
+          <SearchIcon />
+        </button>
+        <input
+          type="text"
+          defaultValue={transcript}
+          className="w-full px-12 py-3 border border-gray-400 rounded-md focus:border-blue-200"
+        />
+        <button className="absolute right-2" onClick={SpeechRecognition.startListening}>
+          <MicrophoneIcon color={listening ? 'red' : 'black'} />
+        </button>
+      </div>
+    </>
   )
 }
