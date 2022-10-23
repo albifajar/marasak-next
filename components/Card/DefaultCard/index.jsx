8f311185ca648c2e87bd986169a1b174feeb992a
winatapranatadev <winatapranata.dev@gmail.com>
2022-10-23 21:43:13 +0700

    fix: hide missing ingridient

--- components/Card/DefaultCard/index.jsx
+++ components/Card/DefaultCard/index.jsx
@@ -17,12 +17,12 @@ export const DefaultCard = ({ url_image, type, title, lessIngredient, cookingTim
       <div className="p-2">
         <span className="text-sm">{type}</span>
         <h1 className="font-semibold line-clamp-2">{title}</h1>
-        <div className="flex flex-col mt-2 space-y-2">
+        <div className="flex flex-col mt-2 mb-1 space-y-2">
           <div className="flex items-center px-2 py-1 space-x-2 bg-gray-200 rounded-full w-max">
             <Image src="/static/icons/fluent_timer-24-regular.svg" width={14} height={14} layout="fixed" alt="" />
             <p className="text-sm">Estimasi {cookingTime}</p>
           </div>
-          <div className="flex items-center px-2 py-1 space-x-2 bg-red-100 rounded-full w-max">
+          <div className="items-center hidden px-2 py-1 space-x-2 bg-red-100 rounded-full w-max">
             <Image src="/static/icons/ic_outline-no-food.svg" width={14} height={14} layout="fixed" alt="" />
             <p className="text-sm">Kurang {lessIngredient} bahan</p>
           </div>
